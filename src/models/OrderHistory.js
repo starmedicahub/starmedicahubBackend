402ba2e9c3d3c1bdef142b6dca20faaff90d86f9
Dhyeybhuva2003 <dhyeybhuva2003@gmail.com>
2024-10-18 22:22:45 +0530

    Done

--- src/models/OrderHistory.js
+++ src/models/OrderHistory.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const orderHistorySchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    cart: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Cart', required: true }],
-    createdAt: { type: Date, default: Date.now }
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  cart: [{ type: mongoose.Schema.Types.ObjectId, ref: "Cart", required: true }],
+  createdAt: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model('OrderHistory', orderHistorySchema);
+module.exports = mongoose.model("OrderHistory", orderHistorySchema);
